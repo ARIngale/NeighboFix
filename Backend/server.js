33d960511c42fea01f573b9b0e29f71e35ea8829
ARIngale <aringale17@gmail.com>
2025-06-19 08:19:50 +0530

    Mongodb Connection established

--- Backend/server.js
+++ Backend/server.js
@@ -13,7 +13,13 @@ app.use(cors())
 app.use(express.json())
 
 
-
+// MongoDB Connection
+mongoose.connect(process.env.MONGODB_URI)
+const db = mongoose.connection
+db.on("error", console.error.bind(console, "connection error:"))
+db.once("open", () => {
+  console.log("Connected to MongoDB")
+})
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`)
