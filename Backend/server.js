b440159a8993c8000716ef478af489716b24eb96
ARIngale <aringale17@gmail.com>
2025-06-19 08:19:19 +0530

    Middleware added

--- Backend/server.js
+++ Backend/server.js
@@ -1,8 +1,20 @@
 const express = require("express")
+const mongoose = require("mongoose")
+const cors = require("cors")
+const dotenv = require("dotenv")
+
+dotenv.config()
 
 const app = express()
-const PORT =  5000
+const PORT = process.env.PORT || 5000
+
+// Middleware
+app.use(cors())
+app.use(express.json())
+
+
+
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`)
-})  
+})
